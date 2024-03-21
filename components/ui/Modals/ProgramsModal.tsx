137c0861e9847b7973c210f21aaaaff11625878a
Drex72 <femidavooo@gmail.com>
2024-03-21 02:05:08 +0100

    fixed the select program modal

--- components/ui/Modals/ProgramsModal.tsx
+++ components/ui/Modals/ProgramsModal.tsx
@@ -10,9 +10,11 @@ interface IProgramsModalProps extends IBaseModalProps {
 export const ProgramsModal = (props: IProgramsModalProps) => {
     const { modalIsMounted, handleClose, createProgram } = props
 
+    const programs = ["Hatchdev", "Nitdev"]
+
     return (
         <ModalLayout isMounted={modalIsMounted} onClose={handleClose}>
-            <div className="px-1 ">
+            <div>
                 <div className="mb-4 flex items-center justify-between border-b-2 border-[#676767_0.5] pb-2 text-[#000000_0.5]">
                     <h2 className="text-xl font-semibold">Select a program</h2>
 
@@ -23,13 +25,19 @@ export const ProgramsModal = (props: IProgramsModalProps) => {
                 </div>
 
                 <div className="mb-8 flex h-full max-h-[400] flex-col gap-4 overflow-y-scroll">
-                    <div className=" shadow-program_card cursor-pointer rounded-sm border-t border-t-primary px-4 py-3 text-base font-normal text-[#101010]">
-                        Hatchdev
-                    </div>
-
-                    <div className=" shadow-program_card cursor-pointer rounded-sm border-t border-t-primary px-4 py-3 text-base font-medium text-[#101010]">
-                        Hatchdev
-                    </div>
+                    {programs.map((program, index) => (
+                        <button
+                            key={index}
+                            className="shadow-program_card group relative mx-auto inline-flex w-[98%] items-center justify-start overflow-hidden rounded bg-white py-3 pl-4 pr-12 text-base font-normal capitalize text-[#101010]  transition-all duration-300 ease-in-out hover:pl-10 hover:pr-6"
+                        >
+                            <span className="absolute left-0 top-0 h-[1px] w-full bg-primary transition-all duration-150 ease-in-out group-hover:h-full"></span>
+                            <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12"></span>
+                            <span className="absolute left-0 -translate-x-12 pl-2.5 duration-200 ease-out group-hover:translate-x-0"></span>
+                            <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                                {program}
+                            </span>
+                        </button>
+                    ))}
                 </div>
 
                 <div className="flex items-end justify-end">
