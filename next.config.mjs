7fb1652c2d9a41e6b94741a4d04c250476c632c8
Drex72 <femidavooo@gmail.com>
2024-04-17 13:04:44 +0100

    reset next config

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import MillionLint from "@million/lint"
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    output: "standalone",
+    images: {
+        domains: ["res.cloudinary.com"],
+    },
+    // basePath: '/nitprofile-client'
+
+}
+// export default MillionLint.next({
+//   rsc: true
+// })(nextConfig);
+
+export default nextConfig
