7e8c83f36a4d1d1e9cb6734ba5f336d63d95c789
Drex72 <femidavooo@gmail.com>
2024-04-17 12:47:37 +0100

    Added config for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,28 @@ const nextConfig = {
         domains: ["res.cloudinary.com"],
     },
     // basePath: '/nitprofile-client'
+    webpack: (config) => {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        config.resolve.fallback = {
+            process: require.resolve('process/browser'),
+            zlib: require.resolve('browserify-zlib'),
+            stream: require.resolve('stream-browserify'),
+            util: require.resolve('util'),
+            buffer: require.resolve('buffer'),
+            asset: require.resolve('assert'),
+        }
+        config.externals.push({ sharp: 'commonjs sharp', canvas: 'commonjs canvas' })
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+              Buffer: ['buffer', 'Buffer'],
+              process: 'process/browser',
+            })
+        )
+        return config
+    }
 
 }
 // export default MillionLint.next({
